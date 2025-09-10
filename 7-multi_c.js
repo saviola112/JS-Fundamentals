f71ae82e64b61046dce6c965c9d602531facf088
saviour ekewuba <ekewubas@gmail.com>
2025-09-10 15:51:09 +0100

    Add 7-multi_c.js script

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,16 +1,16 @@
-vi 7-multi_c.js
+// 7-multi_c.js
+const arg = process.argv[2];
+const n = parseInt(arg, 10);
 
-#!/usr/bin/node
-
-const x = parseInt(process.argv[2]);
-
-if (isNaN(x) || x <= 0) {
-  console.log("Missing number of occurrences");
-} else {
+if (isNaN(n)) {
+  console.log('Missing number of occurrences');
+} else if (n > 0) {
   let i = 0;
-  while (i < x) {
-    console.log("C is fun");
+  let out = '';
+  while (i < n) {
+    out += 'C is fun';
+    if (i !== n - 1) out += '\n';
     i++;
   }
+  console.log(out);
 }
-
